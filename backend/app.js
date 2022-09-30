33f238f08c48f2f4910aaf2f8497f72ca649e043
martindustry <marcin.exn@gmail.com>
2022-09-30 16:38:29 +0200

    Setup database connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = require('./router');
 const app = express();
+require('dotenv/config');
 
 app.use('/api', router);
+
+mongoose.connect(process.env.DB_URI, () => console.log('Connected to DB'));
 
 app.listen(3001);
