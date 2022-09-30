091225206d9eee00626bf2ace498082b804515ba
martindustry <marcin.exn@gmail.com>
2022-09-30 21:00:16 +0200

    Add delete note route

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -45,4 +45,13 @@ notes.put('/:noteId', async (req, res) => {
   }
 });
 
+notes.delete('/:noteId', async (req, res) => {
+  try {
+    const note = await Note.deleteOne({ _id: req.params.noteId });
+    res.status(200).json(note);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 module.exports = notes;
