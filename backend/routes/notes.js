630dd4f9955dd08a29ac369cf88066e26ce495c7
martindustry <marcin.exn@gmail.com>
2022-09-30 20:48:12 +0200

    Add GET route for single note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -24,4 +24,13 @@ notes.post('/', async (req, res) => {
   }
 });
 
+notes.get('/:noteId', async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.noteId);
+    res.status(200).json(note);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
 module.exports = notes;
