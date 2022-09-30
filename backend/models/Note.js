40724e4e9e9ca68a2dbfb09bb1697ddefb244092
martindustry <marcin.exn@gmail.com>
2022-09-30 17:28:58 +0200

    Add models

---
+++ backend/models/Note.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
